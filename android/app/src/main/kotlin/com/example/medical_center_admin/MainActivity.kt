7abddb7f87553bb949a81d25db191d8b8495a3cd
Sari Shehada <sarishehada@gmail.com>
2024-03-28 09:08:40 +0300

    Initial Commmit

---
+++ android/app/src/main/kotlin/com/example/medical_center_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medical_center_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:medical_center_admin/application.dart';
+
+void main() {
+  runApp(const Application());
+}
